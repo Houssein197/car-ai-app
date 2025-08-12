3881e3cfbde1132a00fda430d90cb127cf455963
Abdulhakim houssein <abdulhakimhoussein@Abdulhakims-MacBook-Air-2.local>
2025-08-12 18:36:34 +0100

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { Analytics } from "@vercel/analytics/next";
 import theme from "../theme";
 
 export default function App({ Component, pageProps }) {
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
